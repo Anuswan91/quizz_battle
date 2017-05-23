6adcf4eb22268a1f03d4398983189fd4da64a945
busquets <alexis.pointurier@gmail.com>
2017-05-23 12:55:54 +0200

    Ajout notification & structure index

--- pages/index.php
+++ pages/index.php
@@ -1,34 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="icon" href="../../favicon.ico">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Quiz Battle</title>
-	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+<?php
+    include '../includes/header.php';
+    include '../php/Notif.php';
+?>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
- <body>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="../css/style.css" rel="stylesheet">
+<body>
   <div class="site-wrapper">
     <div class="site-wrapper-inner">
       <div class="cover-container">
         <div class="masthead clearfix">
           <div class="inner">
-            <img class="nav-logo" src="ressources/logo_quiz.png"/>
+            <img class="nav-logo" src="../ressources/logo_quiz.png"/>
             <form class="nav-login">
                 <input type="text" placeholder="Pseudo" name="username" class="login-text"/>
                 <input type="password" placeholder="Mot de passe" name="pseudo" class="login-text"/>
@@ -46,7 +29,7 @@
                     <a href="#" title="S'inscrire" class="btn-home">S'inscrire</a>
                 </div>
                 <div class="col-sm-6 col-home-right">
-                    <img class="img-home" src="ressources/think.png" />
+                    <img class="img-home" src="../ressources/think.png" />
                 </div>
             </div>
         </div>
@@ -55,7 +38,8 @@
   </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-  <script src="../../dist/js/bootstrap.min.js"></script>
-  <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  <?php
+    include '../includes/footer.php';
+  ?>
   </body>
 </html>
---
+++ php/Notif.php
@@ -0,0 +1,29 @@
+<div class="container-info">
+    <div class="div-info-base">
+        <span class="txt-info"></span>
+        <img class="img-info" onclick="closeInfoMsg();" height="15px" src="../ressources/close.png"/>
+    </div>
+</div>
+<script>
+    function closeInfoMsg(){
+        var info = document.getElementsByClassName('div-info-actif');
+        info[0].className = 'div-info-base';
+    }
+    function notif(chaine, codeColor = 1){
+        document.getElementsByClassName('txt-info')[0].innerHTML = chaine;
+        var color;
+        switch(codeColor) {
+            case 1:
+                color = 'rgba(90, 200, 90, 0.7)';
+                break;
+            case 2:
+                color = 'rgba(200, 90, 90, 0.7)';
+                break;
+            default:
+                color = 'rgba(90, 200, 90, 0.7)';
+        }
+        document.getElementsByClassName('div-info-base')[0].style.backgroundColor = color;
+        document.getElementsByClassName('div-info-base')[0].className = 'div-info-actif';
+    }
+    notif('Yo gros test mamene du salle inchallah tg gros fdp');
+</script>
