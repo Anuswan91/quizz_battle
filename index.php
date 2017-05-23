9e1073efa5ac61aa5f82cb9b01563c2b0ee25a3f
busquets <alexis.pointurier@gmail.com>
2017-05-23 12:19:05 +0200

    Finalisation page d'accueil

--- index.php
+++ index.php
@@ -11,17 +11,29 @@ include 'includes/header.php'
   <div class="site-wrapper">
     <div class="site-wrapper-inner">
       <div class="cover-container">
-        <div class="header clearfix">
+        <div class="masthead clearfix">
           <div class="inner">
             <img class="nav-logo" src="ressources/logo_quiz.png"/>
             <form class="nav-login">
                 <input type="text" placeholder="Pseudo" name="username" class="login-text"/>
                 <input type="password" placeholder="Mot de passe" name="pseudo" class="login-text"/>
-                <input type="submit" class="button-go" value="Go"/>
+                <input type="submit" class="btn-go" value="Go"/>
             </form>
           </div>
         </div>
         <div class="inner cover">
+            <div class="row home-container">
+                <div class="col-sm-6 col-home-left">
+                    <h1>Tester vos connaissances !</h1>
+                    <a href="#" title="Jouer" class="btn-home">Jouer en ligne</a><br/>
+                    <a href="#" title="Amis" class="btn-home">Jouer avec des amis</a><br/><br/>
+                    <h1>Pas encore membre ?</h1>
+                    <a href="#" title="S'inscrire" class="btn-home">S'inscrire</a>
+                </div>
+                <div class="col-sm-6 col-home-right">
+                    <img class="img-home" src="ressources/think.png" />
+                </div>
+            </div>
         </div>
       </div>
     </div>
