c0044e67f71fd2bb8bc6d4e03402bcb481bd9641
Maxime <dufour.maxime21@gmail.com>
2017-05-23 14:49:36 +0200

    Séparation gameView et gameController

--- pages/gameView.php
+++ pages/gameView.php
@@ -7,45 +7,14 @@
 
 	<body>
 		<?php
-			//On vérifie si les session sont déja activées
-			if(session_id() == null){
-				session_start();
-			}
-
-
-
-			include "../includes/DB.php"; //Connexion a la base de données
+			include "../php/gameController.php"; //Connexion a la base de données
 		?>
 
-		<?php 
-
-			$qst_theme_id = 4;
-			$sql_question = "SELECT * FROM question WHERE qst_theme_id = ".$qst_theme_id;			
-			$res_question = $bdd->query($sql_question)->fetchAll();
-			$rand_question = rand(0, sizeof($res_question)-1);
-		?>
 		<div id="question">
 		<?php 
 			echo $res_question[$rand_question]['qst_text'];
 		?>
 		</div>
-
-		<?php
-
-			$sql_answer = "SELECT * FROM answer WHERE ans_question_id = ".$res_question[$rand_question]['qst_id'];
-			$res_answer = $bdd->query($sql_answer)->fetchAll();
-
-			function uniq_rand($min,$max,$tab)
-			{
-    			do $rand=rand($min,$max); while(in_array($rand,$tab));
-    			return $rand;
-			}
-
-			$rand_answer = array();
-			
-			for ($i = 0; $i < 4; $i++)
-				$rand_answer[] = uniq_rand(0, 3, $rand_answer);
-		?>
 
 		<form action="answerView.php">
 			<button name='rep1' type='submit'><?php echo $res_answer[$rand_answer[0]]['ans_text']?></button>
