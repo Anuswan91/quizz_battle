753638e55bb969e510e929c32ae190b9dd4f9f7e
Maxime <dufour.maxime21@gmail.com>
2017-05-23 12:56:07 +0200

    Implémentation questions
    Implémentation des questions

--- includes/DB.php
+++ includes/DB.php
@@ -8,7 +8,8 @@ try {
     //$bdd = new PDO('mysql:host=tp-sgbd;dbname=vblum_a;charset=utf8', 'vblum_a', 'vblum_a');
     //Pour se connecter en local ailleurs
     //$bdd = new PDO('mysql:host=localhost;dbname=risk;charset=utf8', 'root', 'root');
-    $bdd = new PDO('mysql:host=localhost;dbname=quizz_battle;charset=utf8', 'root', '');
+    //$bdd = new PDO('mysql:host=localhost;dbname=quizz_battle;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_9732ace84ff668b;charset=utf8', 'bf583c4b055e2d', '8d9c7be6');
 
     // Developpement
     //$bdd = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_9732ace84ff668b;charset=utf8', 'bf583c4b055e2d', '8d9c7be6');
--- pages/gameView.php
+++ pages/gameView.php
@@ -12,14 +12,46 @@
 				session_start();
 			}
 
+
+
 			include "../includes/DB.php"; //Connexion a la base de données
 		?>
 
+		<?php 
+
+			$qst_theme_id = 4;
+			$sql_question = "SELECT * FROM question WHERE qst_theme_id = ".$qst_theme_id;			
+			$res_question = $bdd->query($sql_question)->fetchAll();
+			$rand_question = rand(0, sizeof($res_question)-1);
+		?>
+		<div id="question">
+		<?php 
+			echo $res_question[$rand_question]['qst_text'];
+		?>
+		</div>
+
+		<?php
+
+			$sql_answer = "SELECT * FROM answer WHERE ans_question_id = ".$res_question[$rand_question]['qst_id'];
+			$res_answer = $bdd->query($sql_answer)->fetchAll();
+
+			function uniq_rand($min,$max,$tab)
+			{
+    			do $rand=rand($min,$max); while(in_array($rand,$tab));
+    			return $rand;
+			}
+
+			$rand_answer = array();
+			
+			for ($i = 0; $i < 4; $i++)
+				$rand_answer[] = uniq_rand(0, 3, $rand_answer);
+		?>
+
 		<form action="answerView.php">
-			<button name='rep1' type='submit'>Reponse 1</button>
-			<button name='rep2' type='submit'>Reponse 2</button>
-			<button name='rep3' type='submit'>Reponse 3</button>
-			<button name='rep4' type='submit'>Reponse 4</button>
+			<button name='rep1' type='submit'><?php echo $res_answer[$rand_answer[0]]['ans_text']?></button>
+			<button name='rep2' type='submit'><?php echo $res_answer[$rand_answer[1]]['ans_text']?></button>
+			<button name='rep3' type='submit'><?php echo $res_answer[$rand_answer[2]]['ans_text']?></button>
+			<button name='rep4' type='submit'><?php echo $res_answer[$rand_answer[3]]['ans_text']?></button>
 		</form>
 
 	</body>
