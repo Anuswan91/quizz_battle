1e9ad79c08daec32de247017a93b52e7226fdec2
TZCN4813 <luc.bettuzzi@orange.com>
2017-05-23 16:42:01 +0200

    fin gestion user session reprise

--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,14 @@
 <?php
     include '../includes/header.php';
     include '../php/Notif.php';
+    
+    //$_SESSION['Auth']['plr_pseudo'] = "jhfze";
+   // die(var_dump($_SESSION['Auth']));
+    if(isset($_GET['logout']) && $_GET['logout'] != null)
+    {
+    	$_SESSION['Auth'] = null;
+    }
+    
  ?>
 
 <link href="../css/style.css" rel="stylesheet">
@@ -11,16 +19,21 @@
         <div class="masthead clearfix">
           <div class="inner">
            <img class="nav-logo" src="../ressources/logo_quiz.png"/>
-           <div class="nav-login">
+           <?php
+            if (isset($_SESSION['Auth']) && !empty($_SESSION['Auth']))
+            {?>
+            <div class="nav-login">
                 <span class="username">Pseudo</span>
                 <a href="#" class="btn-go">ADMIN</a>
-                <a href="#" class="btn-go" onclick="notif('Déconnecté', 1);">LOGOUT</a>
-            </div>
-           <!-- <form class="nav-login" action="../php/userLoginController.php"  method="post" name="login">
+                <a href="?logout=true" class="btn-go" onclick="notif('Déconnecté', 1);">LOGOUT</a>
+            </div><?php 
+            }else{?>
+            <form class="nav-login" action="../php/userLoginController.php"  method="post" name="login">
                 <input type="text" placeholder="Pseudo" name="pseudo" class="login-text"/>
                 <input type="password" placeholder="Mot de passe" name="password" class="login-text"/>
                 <input type="submit" class="btn-go" value="Go"/>
-            </form>-->
+            </form>
+            <?php }?>
 
           </div>
         </div>
@@ -31,7 +44,7 @@
                     <a href="#" title="Jouer" class="btn-home">Jouer en ligne</a><br/>
                     <a href="#" title="Amis" class="btn-home">Jouer avec des amis</a><br/><br/>
                     <?php
-                    	if (!isset($_SESSION['Auth']) && $_SESSION['Auth']!= null)
+                    	if (!isset($_SESSION['Auth']))
                     	{
                     		?>
                     		<h1>Pas encore membre ?</h1>
