74c08776c081b308155c52e9cc1bc0cec50725b6
busquets <alexis.pointurier@gmail.com>
2017-05-25 17:08:18 +0200

    Design page admin

--- pages/adminView.php
+++ pages/adminView.php
@@ -2,94 +2,129 @@
 include '../php/AdminController.php'; 
 include '../includes/header.php';
 ?>
-
-<!-- Formulaire nouveau thème -->
-<form action="../php/adminController.php" method="post">
-  <input type="text" placeholder="Thème" name="nt_theme" required>
-  <input type="color" value="#FFFFFF" name="nt_color" required>
-  <input type="submit" value="Submit">
-</form>
-
-<!-- Tableau des thèmes -->
-<table class="table">
-  <tr>
-    <th>Identifiant</th>
-    <th>Nom</th>
-    <th>Actions</th>
-  </tr>
-  <?php
-  	foreach ($themes as $theme) {
-		echo '<tr>';
-		echo '<td>'.$theme['thm_id'].'</td>';
-		echo '<td bgcolor="'.$theme['thm_color'].'">'.$theme['thm_name'].'</td>';
-		echo '<td>';
-		echo '<a href="../php/adminController.php?dt='.$theme['thm_id'].'">';
-		echo '<span class="glyphicon glyphicon-trash">';
-		echo '</a>';
-		echo '</td>';
-		echo '</tr>';
-		echo '</tr>';
-	}
-  ?>
-</table>
-
-<!-- Formulaire nouvelle question -->
-<form action="../php/adminController.php" method="post">
-	<select name="nq_theme" required>
-	<?php
-		foreach ($themes as $theme) {
-			echo '<option value="'.$theme['thm_id'].'">';
-			echo $theme['thm_name'];
-			echo '</option>';
-		}	
-	?>
-	</select><br>
-	<input type="text" placeholder="Question" name="nq_question" required><br>
-	<input type="checkbox" name="nq_correctA">
-	<input type="text" placeholder="Réponse A" name="nq_answerA" required><br>
-	<input type="checkbox" name="nq_correctB">
-	<input type="text" placeholder="Réponse B" name="nq_answerB" required><br>
-	<input type="checkbox" name="nq_correctC">
-	<input type="text" placeholder="Réponse C" name="nq_answerC" required><br>
-	<input type="checkbox" name="nq_correctD">
-	<input type="text" placeholder="Réponse D" name="nq_answerD" required><br>
-	<input type="submit" value="Submit">
-</form>
-
-<!-- Tableau des questions -->
-<table class="table">
-  <tr>
-    <th>Identifiant</th>
-    <th>Question</th>
-    <th>Thème</th>
-    <th>Réponses</th>
-    <th>Actions</th>
-  </tr>
-  <?php
-	  foreach ($questions as $key => $question) {
-	  	echo '<tr>';
-		echo '<td>'.$key.'</td>';
-		echo '<td>'.$question['text'].'</td>';
-		echo '<td>'.$question['theme'].'</td>';
-		echo '<td>';
-		echo '<ul>';
-		foreach ($question['answers'] as $answer) {
-			if ($answer['ans_correct']) {
-				echo '<li><b>'.$answer['ans_text'].'</b></li>';
-			}
-			else {
-				echo '<li>'.$answer['ans_text'].'</li>';
-			}
-		}
-		echo '</ul>';
-		echo '</td>';
-		echo '<td>';
-		echo '<a href="../php/adminController.php?dq='.$key.'">';
-		echo '<span class="glyphicon glyphicon-trash">';
-		echo '</a>';
-		echo '</td>';
-		echo '</tr>';
-	}
-  ?>
-</table>
+<link href="../css/styleAdmin.css" rel="stylesheet">
+<body>
+<div class="site-wrapper">
+	<div class="site-wrapper-inner">
+		<div class="cover-container">
+			<div class="masthead clearfix">
+				<div class="inner">
+					<a href="index.php"><img class="nav-logo" src="../img/logo_quiz.png"/></a>
+					<div class="nav-login">
+						<span class="username"><?= $_SESSION['Auth']['plr_pseudo']?></span>
+						<a href="#" class="btn-go">ADMIN</a>
+						<a href="?logout=true" class="btn-go" onclick="notif('Déconnecté', 1);">LOGOUT</a>
+					</div>
+				</div>
+			</div>
+			<div class="inner cover">
+				<div class="row home-container">
+					<div class="col-sm-6">
+						<h1>Nouveau théme:</h1></br>
+						<form action="../php/adminController.php" method="post">
+							<input type="text" placeholder="Thème" name="nt_theme" class="login-text" required>
+							<input type="color" value="#FFFFFF" name="nt_color" class="theme-color-picker login-text" required>
+							<input type="submit" class="btn-go" value="Go">
+						</form></br></br>
+						<h1>Liste des thémes existants: </h1></br>
+						<div class="themes">
+							<?php
+							foreach ($themes as $theme) {
+								?>
+								<div class="themes-display" style="background-color: <?php echo $theme['thm_color']; ?>; opacity:0.7;">
+									<span class="glyphicon glyphicon-trash icon-trash" id="<?php echo $theme['thm_id']; ?>" onclick="window.location.href = '../php/adminController.php?dt= <?php echo $theme['thm_id']; ?>';" ></span>
+									<span ><?php echo $theme['thm_name']; ?></span>
+								</div>
+							<?php }?>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="row">
+							<div class="col-sm-12">
+								<h1>Nouvelle question:</h1></br>
+								<form action="../php/adminController.php" method="post" class="new-question">
+									Théme:
+									<select name="nq_theme" required  class="login-text">
+										<?php
+										foreach ($themes as $theme) {
+											echo '<option value="'.$theme['thm_id'].'">';
+											echo $theme['thm_name'];
+											echo '</option>';
+										}
+										?>
+									</select><br/>
+									Question: <input type="text" placeholder="Question" name="nq_question" class="login-text" required/><br/>
+									Réponse A: <input type="text" placeholder="Réponse A" name="nq_answerA"  class="login-text" required/>
+									<input type="checkbox" name="nq_correctA"><br/>
+									Réponse B: <input type="text" placeholder="Réponse B" name="nq_answerB" class="login-text"  required/>
+									<input type="checkbox" name="nq_correctB"><br/>
+									Réponse C: <input type="text" placeholder="Réponse C" name="nq_answerC" class="login-text"  required/>
+									<input type="checkbox" name="nq_correctC"><br/>
+									Réponse D: <input type="text" placeholder="Réponse D" name="nq_answerD" class="login-text"  required/>
+									<input type="checkbox" name="nq_correctD"><br/>
+									<input type="submit" value="Envoyer" class="btn-home"/>
+								</form>
+							</div>
+						</div>
+						</br><h1>Liste questions:</h1></br>
+						<div class="table-qst">
+							<div class="row title-ans">
+								<div class="col-sm-5">
+									Question
+								</div>
+								<div class="col-sm-2">
+									Théme
+								</div>
+								<div class="col-sm-4">
+									Réponse
+								</div>
+								<div class="col-sm-1">
+								</div>
+							</div>
+							<?php
+							foreach ($questions as $key => $question) { ?>
+								<div class="row qst-unit">
+									<div class="col-sm-5">
+										<span class="qst-txt"><?php echo $question['text']; ?></span>
+									</div>
+									<div class="col-sm-2">
+										<code><?php echo $question['theme']; ?></code>
+									</div>
+									<div class="col-sm-4">
+										<?php foreach ($question['answers'] as $answer) {
+											if ($answer['ans_correct']) {
+												?>
+												<span
+													class="glyphicon glyphicon-ok"> <?php echo $answer['ans_text']; ?></span>
+												<br/>
+												<?php
+											} else {
+												?>
+												<span
+													class="glyphicon glyphicon-remove"> <?php echo $answer['ans_text']; ?></span>
+												<br/>
+												<?php
+											}
+										}
+										?>
+									</div>
+									<div class="col-sm-1">
+										<span class="glyphicon glyphicon-trash icon-trash" onclick="window.location.href = '../php/adminController.php?dq= <?php echo $key; ?>';"></span>
+									</div>
+								</div>
+								<?php
+							}
+							?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+include '../includes/footer.php';
+?>
+</body>
+</html>
 
